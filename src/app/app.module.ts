eeca061d608a6a79c64cb64d19f92689d7412221
MxrioHuerta <marioyagami23@gmail.com>
2024-10-27 10:29:02 -0600

    Restructure components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,13 @@ import { provideAuth,getAuth } from '@angular/fire/auth';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {environment} from '../environments/environment';
-import {LoginComponent} from "./pages/login/login.component";
-import {BrowseComponent} from './pages/browse/browse.component';
-import {HeaderComponent} from './core/components/header/header.component';
-import {BannerComponent} from './core/components/banner/banner.component';
+import {LoginComponent} from "./components/login/login.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    BrowseComponent,
-    HeaderComponent,
-    BannerComponent
   ],
   imports: [
     BrowserModule,
